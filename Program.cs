88dd21647e388278d1370d169975a7b862836f08
haku-melon <Q8jhmain@outlook.jp>
2026-08-06 23:27:24 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("4つの変数を宣言");
+
+string name="メロン"; // 名前
+Console.WriteLine($"名前: {name}");
+
+int age = 3;
+Console.WriteLine($"年齢: {age}");
+
+double height=1.7; // 身長
+Console.WriteLine($"身長: {height}");
+
+bool islikeProgramming=true; // プログラミングが好きかどうか
+Console.WriteLine($"プログラミングが好き: {islikeProgramming}");
